118d19be13044852b78bb96b0f73d05b32b95231
MohammadReza Omrani <52138433+mohammad-reza-omrani@users.noreply.github.com>
2025-02-07 09:28:09 -0800

    Add files via upload

---
+++ f-String_Variables-in-Strings_exp2.py
@@ -0,0 +1,4 @@
+First_Name = "ada"
+Last_Name = "lovelace"
+Full_Name = f"{First_Name} {Last_Name}"
+print(f"Hello, {Full_Name.title()}!")
---
+++ f-String_Variables-in-Strings_exp3.py
@@ -0,0 +1,5 @@
+First_Name = "ada"
+Last_Name = "lovelace"
+Full_Name = f"{First_Name} {Last_Name}"
+Message = f"Hello, {Full_Name.title()}!"
+print(Message)
